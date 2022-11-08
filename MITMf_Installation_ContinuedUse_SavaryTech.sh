dd3bc9009db1c90c8ec34b4563bb003a95854d59
Benjy-cyber <112399570+Benjy-cyber@users.noreply.github.com>
2022-11-07 18:00:58 -0600

    Update 1.2

--- MITMf_Installation_ContinuedUse_SavaryTech.sh
+++ MITMf_Installation_ContinuedUse_SavaryTech.sh
@@ -2,7 +2,6 @@
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
 source `which virtualenvwrapper.sh`
 mkvirtualenv MITMf -p /usr/bin/python2.7
-git clone https://github.com/byt3bl33d3r/MITMf
 git submodule init && git submodule update --recursive 
 
 pip install pyinotify
